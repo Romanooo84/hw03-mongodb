e0ec94a53c55c1dbf9549d4dc83a9df2dfc6926a
Pisarski Roman <133641494+Romanooo84@users.noreply.github.com>
2024-06-26 18:56:35 +0200

    start

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
+require('dotenv').config()
+
+const contactsRouter = require('./routes/api/contacts')
+
+const app = express()
+
+const corsOptions = {
+  origin:[`https://www.google.pl`]
+}
+
+app.use(logger('combined'))
+app.use(cors(corsOptions))
+app.use(express.json())
+
+app.use('/api/contacts', contactsRouter)
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found' })
+})
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message })
+})
+
+module.exports = app
